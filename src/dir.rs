79ecd2041009c6d24ecaf6d2f5a79a8e050861e1
Luna <luna@desroches.org>
2026-08-12 20:05:33 -0400

    Implement writing directory contents to web server

--- src/dir.rs
+++ src/dir.rs
@@ -1,16 +1,26 @@
-use std::{
-    fs::{DirEntry, read_dir},
-    io::Error,
-    path::{Path, PathBuf},
-};
+pub mod dir_reader {
+    use std::{
+        fs::{DirEntry, read_dir},
+        io::{self, Error},
+        path::Path,
+    };
 
-pub fn dir_reader() -> Result<Vec<PathBuf>, Error> {
-    let path = Path::new(".");
-    let mut entries: Vec<PathBuf> = read_dir(path)?
-        .map(|res| res.map(|e: DirEntry| e.path()))
-        .collect::<Result<Vec<_>, Error>>()?;
+    pub fn dir_reader() -> Result<Vec<String>, Error> {
+        let path = Path::new(".");
+        let mut entries: Vec<String> = read_dir(path)?
+            .map(|res| res.map(|e: DirEntry| String::from(e.path().to_str().unwrap())))
+            .collect::<Result<Vec<_>, Error>>()?;
 
-    entries.sort();
+        entries.sort();
 
-    Ok(entries)
+        Ok(entries)
+    }
+
+    pub fn list_files() -> io::Result<String> {
+        let files = dir_reader()?;
+
+        let file_list = files.as_slice().join("\n");
+
+        Ok(file_list)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,11 @@ use std::{
     io::{self, Write},
     net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
 };
-
 mod dir;
-
-use dir::dir_reader;
+use dir::dir_reader::list_files;
 
 fn main() -> io::Result<()> {
     let _ = open_conn();
-
-    Ok(())
-}
-
-fn _list_files() -> io::Result<()> {
-    // Impl handler list files in directory
-    let files = dir_reader()?;
-    for file in files {
-        println!("{:?}", file.as_path());
-    }
-
     Ok(())
 }
 
@@ -27,24 +14,23 @@ fn open_conn() -> io::Result<()> {
     let addr: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
     let socket: SocketAddrV4 = SocketAddrV4::new(addr, 8080);
     let listener: TcpListener = TcpListener::bind(socket)?;
+    let files = list_files()?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(st) => {
-                _ = handle(st);
+                _ = handle(st, &files);
             }
             Err(_) => {
                 println!("An error has occured");
             }
         };
     }
-
     Ok(())
 }
 
-fn handle(mut stream: TcpStream) -> io::Result<()> {
-    let msg: &str = "<h2>Dir:</h2>
-                    <p>file_name.ext</p>";
+fn handle(mut stream: TcpStream, files: &str) -> io::Result<()> {
+    let msg = format!("<h2>Directories:</h2><p>{}</p>", files);
 
     let lines: &[String] = &[
         format!("HTTP/1.1 200 OK"),
